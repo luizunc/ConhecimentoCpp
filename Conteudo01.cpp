5e4d5c6c7640ee7ba57d23d79e4b3f4c085e7e7e
luizunc <redgameplay33@gmail.com>
2025-10-20 13:03:56 -0300

    New: Arquivo 01, criação simples de Hello World

---
+++ Conteudo01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+	// Altera cor do console e gera a mensagem "Hello, World!".
+	system("color 0D");
+	std::cout << "Hello, World!" << std::endl;
+	system("pause");
+	return 0;
+
+}
